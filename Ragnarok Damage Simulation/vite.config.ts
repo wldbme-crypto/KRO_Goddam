a569df5e355e77e5a55c5600c8645d2ea497f0df
wldbme-crypto <wldbme@gmail.com>
2026-02-25 10:39:47 +0900

    Update vite.config.ts

--- Ragnarok Damage Simulation/vite.config.ts
+++ Ragnarok Damage Simulation/vite.config.ts
@@ -4,17 +4,22 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/KRO_Goddam/',  // 
+  base: '/KRO_Goddam/',
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['figma:*'],
+      output: {
+        external: ['figma:*'],
+      },
     },
   },
 })
